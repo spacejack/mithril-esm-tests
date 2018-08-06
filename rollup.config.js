9c413cdb64aea95fbf11bb7dac5389b1f64cd46a
spacejack <spacejack@gmail.com>
2018-08-06 17:18:56 -0400

    Rollup bundling fixes

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,24 @@
-import typescript from 'rollup-plugin-typescript2';
+import typescript from 'rollup-plugin-typescript2'
+import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
 
 export default {
   input: './src/index.ts',
   output: {
     file: './public/app.js',
-    output: 'iife'
+    format: 'iife'
   },
 
   plugins: [
+    nodeResolve({
+      jsnext: true,
+      main: true
+    }),
+    commonjs({
+      include: 'node_modules/**',
+    }),
     typescript({
       tsconfig: 'tsconfig.rollup.json'
-    })
+    }),
   ]
 }
