1b9429375a15aa5f99b352776088946b0e8491f9
spacejack <spacejack@gmail.com>
2018-08-06 16:54:23 -0400

    Add rollup test

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: './src/index.ts',
+  output: {
+    file: './public/app.js',
+    output: 'iife'
+  },
+
+  plugins: [
+    typescript({
+      tsconfig: 'tsconfig.json'
+    })
+  ]
+}
